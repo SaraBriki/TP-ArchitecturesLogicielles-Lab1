0302e46dfb80e8cc565e7bc49fb911fbbd862bc6
Alaeddine <alaeddinefattouch@insat.u-carthage.tn>
2023-02-10 10:42:04 +0100

    added SRP solution

---
+++ SRP/src/com/directi/training/srp/solution/CarManager.java
@@ -0,0 +1,36 @@
+package com.directi.training.srp.exercise;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CarManager
+{
+    private List<Car> _carsDb;
+
+    public CarManager(List<Car> _carsDb) {
+        this._carsDb = _carsDb;
+    }
+
+    public String getCarsNames()
+    {
+        StringBuilder sb = new StringBuilder();
+        for (Car car : _carsDb) {
+            sb.append(car.getBrand());
+            sb.append(" ");
+            sb.append(car.getModel());
+            sb.append(", ");
+        }
+        return sb.substring(0, sb.length() - 2);
+    }
+
+    public Car getBestCar()
+    {
+        Car bestCar = null;
+        for (Car car : _carsDb) {
+            if (bestCar == null || car.getModel().compareTo(bestCar.getModel()) > 0) {
+                bestCar = car;
+            }
+        }
+        return bestCar;
+    }
+}
